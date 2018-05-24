88091cf30cab2f8538c795adfc43fef94b7e4299
Unknown <marchon.jbaptiste@gmail.com>
2018-05-25 00:24:21 +0200

    Module LDAP avec UnboundID SDK
    La librairie est fournie dans le dossier "librairies"

--- sync-tool/LDAPclient.java
+++ sync-tool/LDAPclient.java
@@ -1,38 +1,43 @@
-import javax.naming.*;
-import java.util.*;
-import javax.naming.ldap.*;
-import javax.naming.directory.*;
-
+import com.unboundid.ldap.sdk.*;
 
 public class LDAPclient {
 
-    public static void main(String args[]) throws NamingException
-    {
-        try {
-            Hashtable<String, String> env = new Hashtable<String, String>();
-            String domain = "DOMAIN0\\"; //domain name of AD server (ex: domain.com.sg), not domain name of website
-            String username = "bvincent"; //user domain to login PC
-            String password = "password";
-            String securityPrincipal = "cn=Directory Manager"; // or username + "@" + "domain.com.sg";
+    /**
+     * @param args
+     * @throws LDAPException
+     */
+    public static void main(String[] args) throws LDAPException {
 
-            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-            env.put(Context.PROVIDER_URL, "ldap://localhost.localdomain:1389"); //IP and port of LDAP
-            env.put(Context.SECURITY_AUTHENTICATION, "simple");
-            env.put(Context.SECURITY_PRINCIPAL, securityPrincipal); // replace with user DN
-            env.put(Context.SECURITY_CREDENTIALS, password);
 
-            /*DirContext dcx = new InitialDirContext(env);*/
-            //InitialLdapContext ctx = new InitialLdapContext(env, null);
-            LdapContext ctx = new InitialLdapContext(env, null);
-            System.out.println("Authentication Success!");
-            //return true;
+        LDAPConnection connection = new LDAPConnection("localhost.localdomain", 1389, "cn=Directory Manager","password");
 
-        } catch (NamingException ex) {
-            System.out.println("Authentication failed: " + ex.toString());
-            //return false;
-        } catch (Exception ex) {
-            System.out.println("Authentication failed ! Exception: " + ex.toString());
-            //return false;
+        Filter filter = Filter.create("(objectClass=*)");
+        SearchRequest searchRequest = new SearchRequest("ou=Professors,ou=People,dc=isep,dc=fr", SearchScope.SUB, filter,"mail","telephoneNumber","cn");
+        SearchResult searchResult;
+        try
+        {
+            searchResult = connection.search(searchRequest);
+
+            for (SearchResultEntry entry : searchResult.getSearchEntries())
+            {
+                String name = entry.getAttributeValue ("cn");
+                String mail = entry.getAttributeValue("mail");
+                String phone = entry.getAttributeValue("telephoneNumber");
+                System.out.println(name + "\n" + mail + "\n" + phone);
+            }
+
         }
+        catch (LDAPSearchException lse)
+        {
+            // The search failed for some reason.
+            searchResult = lse.getSearchResult();
+            ResultCode resultCode = lse.getResultCode();
+            String errorMessageFromServer = lse.getDiagnosticMessage();
+
+        }
+
     }
+
+
 }
+
