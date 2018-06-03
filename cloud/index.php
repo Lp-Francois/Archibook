283207f515b8eb725a257340e892512f7139ee59
François <32224751+AsterYujano@users.noreply.github.com>
2018-06-03 20:15:49 +0200

    add logo

--- cloud/index.php
+++ cloud/index.php
@@ -14,7 +14,8 @@
   </head>
   <body class="container mt-5 center">
     <div>
-    	<h1>_ Archibook _</h1>
+    	<!--<h1>_ Archibook _</h1>-->
+        <img src="archibook_logo.png" width="300px" style="text-align: center; margin: auto; display: flex;">
     	<hr>
     </div>
 
